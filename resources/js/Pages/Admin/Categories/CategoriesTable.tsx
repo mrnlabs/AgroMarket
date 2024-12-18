5c25812dda0f661579a3b9a8573b5eafb9bbc0cf
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-12-19 01:17:28 +0200

    Update category table

--- resources/js/Pages/Admin/Categories/CategoriesTable.tsx
+++ resources/js/Pages/Admin/Categories/CategoriesTable.tsx
@@ -11,12 +11,7 @@ const CategoriesTable: React.FC<CategoriesTableProps> = ({ categories }) => {
         <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
             <thead className="bg-gray-50 dark:bg-gray-700">
                 <tr>
-                    <th scope="col" className="py-3 px-4 pe-0">
-                        <div className="flex items-center h-5">
-                            <input id="table-search-checkbox-all" type="checkbox" className="form-checkbox rounded"/>
-                            <label htmlFor="table-search-checkbox-all" className="sr-only">Checkbox</label>
-                        </div>
-                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">#</th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Product Count</th>
                     <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
@@ -25,11 +20,8 @@ const CategoriesTable: React.FC<CategoriesTableProps> = ({ categories }) => {
             <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
             {categories && categories.map((category) => (
                 <tr key={category.id}>
-                    <td className="py-3 ps-4">
-                        <div className="flex items-center h-5">
-                            <input id="table-search-checkbox-1" type="checkbox" className="form-checkbox rounded"/>
-                            <label htmlFor="table-search-checkbox-1" className="sr-only">Checkbox</label>
-                        </div>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                    <img className="me-3 rounded-full" src="https://avatars.githubusercontent.com/u/56942052?s=96&v=4" width="40" alt="Generic placeholder image"/>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{category.name}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">45</td>
