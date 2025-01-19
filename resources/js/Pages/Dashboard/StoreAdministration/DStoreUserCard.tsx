89ccea5ecb28a91da79e77d51fa9857b4d6c2276
unknown <mazisimsebele18@gmail.com>
2025-01-19 16:53:55 +0200

    feat: Add user deletion functionality and enhance dark mode styles in user card component

--- resources/js/Pages/Dashboard/StoreAdministration/DStoreUserCard.tsx
+++ resources/js/Pages/Dashboard/StoreAdministration/DStoreUserCard.tsx
@@ -1,10 +1,33 @@
-import { UserCardProps } from '@/types'
+import { Toaster } from '@/Components/ui/toaster';
+import { toast } from '@/hooks/use-toast';
+import { User, UserCardProps } from '@/types'
 import { transcateText } from '@/utils/transcateText';
-import { Link, usePage } from '@inertiajs/react'
-import React from 'react'
+import { Link, router, usePage } from '@inertiajs/react'
+
 
 export default function DStoreUserCard({user}: UserCardProps) {
     const filePath = usePage().props.filePath;
+
+    const deleteUser = (user: User) => {
+        if (confirm('Are you sure you want to delete this user?')) {
+            router.delete(route('users.destroy', user.slug),{
+                onSuccess: () => {
+                    toast({
+                        title: "Success",
+                        description: "User deleted successfully",
+                        variant: "default",
+                    })
+                },
+                onError: () => {
+                    toast({
+                        title: "Error",
+                        description: "Something went wrong",
+                        variant: "destructive",
+                    })
+                }
+            });
+        }
+    }
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="flex justify-end px-4 pt-4">
@@ -17,17 +40,18 @@ export default function DStoreUserCard({user}: UserCardProps) {
         </div>
         <div className="flex flex-col items-center pb-10">
             <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={user.photo_path ? filePath + user.photo_path : 'https://flowbite.com/docs/images/people/profile-picture-3.jpg'} alt="Bonnie image"/>
-            {/* <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{`${user.first_name} ${user.last_name}`}</h5> */}
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{transcateText(`${user.first_name} ${user.last_name}`, 20, '...')}</h5>
             <span className="text-sm text-gray-500 dark:text-gray-400">
                 {user.roles && user.roles.map((role: any) => role.name).join(', ')}</span>
             <div className="flex mt-4 md:mt-6">
                 <Link href={route('users.edit', user.slug)} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Edit</Link>
-                <button className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-red-500 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                <button onClick={() => deleteUser(user)} type='button'
+                 className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-red-500 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                     Delete</button>
             </div>
         </div>
+         <Toaster />
     </div>
   )
 }
